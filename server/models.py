0f3b0174069268d3d5e249ef0c34526c5b8d70a5
muunseo <mkogo90@naver.com>
2025-12-05 00:23:45 +0900

    fix/요청 형식 수정

--- server/models.py
+++ server/models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Literal
 
 class CallInput(BaseModel):
-    seesion_id: str
+    session_id: str 
     text: str
     emotion_label: Literal["anger", "sad", "fear"]
     emotion_score: float
